e98baaee255f4a3ac620abcd1ec4da1a8409b59e
C0deDeveloper <pratyush.mishra2011@outlook.com>
2023-10-25 23:00:21 +0530

    latest bullshit

--- SystemData.cs
+++ SystemData.cs
@@ -14,5 +14,15 @@ namespace Lynox
         public const string OSAcNa = OSName + " " + OSVer; // OS Access Name
 
         public static CosmosVFS fs = new();
+        public static ProcessManager procMgr = new();
+
+        public static void init() => procMgr.AddProc(OSName);
+    }
+
+    internal class ProcessManager
+    {
+        public List<string> ProcessNames = new List<string>();
+        public void AddProc(string procName) => ProcessNames.Add(procName);
+        public void RemProc(string procName) => ProcessNames.Remove(procName);
     }
 }

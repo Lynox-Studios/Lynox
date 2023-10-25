74fdf40d99ef3db90f102ee979c8d528498f4b93
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2023-10-25 17:39:35 +0200

    Update Kernel.cs

--- Kernel.cs
+++ Kernel.cs
@@ -14,6 +14,13 @@ namespace Lynox
 
         protected override void Run()
         {
+
+            if (!Directory.Exists("0:\\bin"))
+            {
+                Booting.diagPrint("FAILED", "Your system has invalid/broken binaries, you will be sent to mode config recovery.");
+                Recovery.ConfigRecovery.START();
+            }
+
             if (!File.Exists("0:\\system\\system_mode.conf"))
             {
                 Booting.diagPrint("FAILED", "Your system has invalid/broken configs, you will be sent to mode config recovery.");
